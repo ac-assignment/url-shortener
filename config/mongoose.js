0a059a95dc017fc3c2b895ca82ddd9863f477fdd
XianEnLin <blueeye851114@gmail.com>
2022-02-08 22:38:47 +0800

    style: 專案名稱微調

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/url_shortener'
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/url-shortener'
 
 mongoose.connect(MONGODB_URI)
 const db = mongoose.connection
